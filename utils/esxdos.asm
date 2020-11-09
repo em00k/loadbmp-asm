ae1a897a6c5313963f6d95e78855e509d108532b
David Saphier <david.saphier@gmail.com>
2020-11-09 18:08:08 +0000

    Update esxdos.asm

--- utils/esxdos.asm
+++ utils/esxdos.asm
@@ -1,4 +1,17 @@
 	
+M_GETSETDRV 		equ $89
+F_OPEN 			equ $9a
+F_CLOSE 		equ $9b
+F_READ 			equ $9d
+F_WRITE 		equ $9e
+F_SEEK 			equ $9f
+F_GET_DIR 		equ $a8
+F_SET_DIR 		equ $a9
+
+FA_READ 		equ $01
+FA_APPEND 		equ $06
+FA_OVERWRITE 		equ $0C
+
 load:
 	
 	push hl  					; save destination 
